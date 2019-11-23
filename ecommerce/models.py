bbb2938413989e7eb8fe7569fe00423e24f76f4e
Muhammad Hammad <hammadarshad834@gmail.com>
2019-11-23 12:16:51 +0500

    Added routes of base.html and home.html

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
 
@@ -6,20 +7,38 @@ class Category(models.Model):
     categoryId = models.AutoField(primary_key=True)
     categoryName = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.categoryName
+
 class Products(models.Model):
     productId = models.AutoField(primary_key=True)
+    productImg = models.ImageField(upload_to='Products', default='default.png')
     productTitle = models.CharField(max_length=50)
     productDescription = models.TextField()
     productRatings = models.FloatField()
-    productPrice = models.FloatField()
     productSales = models.IntegerField()
+    marketPrice = models.FloatField()
+    discountedPrice = models.FloatField()
     productCategory = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-class Comments(models.Model):
-    commentId = models.AutoField(primary_key=True)
-    commentText = models.TextField()
-    productId = models.ForeignKey(Products, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.productTitle
 
-class Cart(models.Model):
-    cartId = models.AutoField(primary_key=True)
-    productId = models.ForeignKey(Products, on_delete=models.CASCADE)
+    def save(self):
+        super().save()
+
+        img = Image.open(self.productImg.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.productImg.path)
+
+# class Comments(models.Model):
+#     commentId = models.AutoField(primary_key=True)
+#     commentText = models.TextField()
+#     productId = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+# class Cart(models.Model):
+#     cartId = models.AutoField(primary_key=True)
+#     productId = models.ForeignKey(Products, on_delete=models.CASCADE)
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello')
+    products = models.Products.objects.all()
+    return render(request, 'ecommerce/home.html', {'products': products})
